009faf75f80bb4310493bb7e1b48e237deda32f8
Álvaro Jiménez Ortiz <32238907+jolvary@users.noreply.github.com>
2025-05-25 16:57:50 +0200

    Quitando mierda

--- funciones.php
+++ funciones.php
@@ -7,58 +7,9 @@ use PDOException;
 use RuntimeException;
 use TypeError;
 
-class DatabaseTcp
-{
-    public static function initTcpDatabaseConnection(): PDO
-    {
-        try {
-            // Note: Saving credentials in environment variables is convenient, but not
-            // secure - consider a more secure solution such as
-            // Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
-            // keep secrets safe.
-            $username = getenv('DB_USER'); // e.g. 'your_db_user'
-            $password = getenv('DB_PASS'); // e.g. 'your_db_password'
-            $instanceHost = getenv('INSTANCE_HOST'); // e.g. '127.0.0.1' ('172.17.0.1' for GAE Flex)
-
-            // Connect using TCP
-            $dsn = sprintf('mysql:host=%s', $instanceHost);
-
-            // Connect to the database
-            $conn = new PDO(
-                $dsn,
-                $username,
-                $password,
-                # ...
-            );
-        } catch (TypeError $e) {
-            throw new RuntimeException(
-                sprintf(
-                    'Invalid or missing configuration! Make sure you have set ' .
-                        '$username, $password, $dbName, and $instanceHost (for TCP mode). ' .
-                        'The PHP error was %s',
-                    $e->getMessage()
-                ),
-                $e->getCode(),
-                $e
-            );
-        } catch (PDOException $e) {
-            throw new RuntimeException(
-                sprintf(
-                    'Could not connect to the Cloud SQL Database. Check that ' .
-                        'your username and password are correct, that the Cloud SQL ' .
-                        'proxy is running, and that the database exists and is ready ' .
-                        'for use. For more assistance, refer to %s. The PDO error was %s',
-                    'https://cloud.google.com/sql/docs/mysql/connect-external-app',
-                    $e->getMessage()
-                ),
-                $e->getCode(),
-                $e
-            );
-        }
-
-        return $conn;
-    }
-}
+$username = getenv('DB_USER'); // e.g. 'your_db_user'
+$password = getenv('DB_PASS'); // e.g. 'your_db_password'
+$instanceHost = getenv('INSTANCE_HOST'); // e.g. '127.0.0.1' ('172.17.0.1' for GAE Flex)
 
 function DBCreation(){
 
